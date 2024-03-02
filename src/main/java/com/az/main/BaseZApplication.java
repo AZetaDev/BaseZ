f9569aa087dccab6a8eebc6466eb9a6889a0b3dd
Alex <azdc1986@gmail.com>
2024-03-02 21:19:05 +0100

    Spring-Boot Application

---
+++ src/main/java/com/az/main/BaseZApplication.java
@@ -0,0 +1,12 @@
+package com.az.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseZApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseZApplication.class, args);
+    }
+}
---
+++ src/test/java/com/az/main/BaseZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BaseZApplicationTests {
+
+    @Test
+    void firstTest() {
+
+    }
+}
