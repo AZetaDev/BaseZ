af1942deaee0f19a88738ff159ec07540efb7b64
Alex <azdc1986@gmail.com>
2024-03-05 19:56:15 +0100

    Config - Logs

--- src/main/java/com/az/main/BaseZApplication.java
+++ src/main/java/com/az/main/BaseZApplication.java
@@ -10,7 +10,22 @@ public class BaseZApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(BaseZApplication.class);
     public static void main(String[] args) {
-        logger.info("Init app");
+        logger.info("Init app - INFO");
+        logger.debug("Init app - DEBUG");
+        logger.warn("Init app - WARN");
+        logger.trace("Init app - TRACE");
+        logger.error("Init app - ERROR");
         SpringApplication.run(BaseZApplication.class, args);
     }
 }
+
+
+/*
+    Next:
+
+    UNIT TESTING:
+        https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
+        https://mvnrepository.com/artifact/org.mockito/mockito-core
+
+    LOMBOK: https://mvnrepository.com/artifact/org.projectlombok/lombok
+ */
