9e47afccf7cea9b226761b9e4bf4149570eb7799
Alex <azdc1986@gmail.com>
2024-03-06 08:17:45 +0100

    Refactor - Main reubicated for componentScan

---
+++ src/main/java/com/az/base/BaseZApplication.java
@@ -0,0 +1,12 @@
+package com.az.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseZApplication {
+//    private static final Logger logger = LoggerFactory.getLogger(BaseZApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(BaseZApplication.class, args);
+    }
+}
