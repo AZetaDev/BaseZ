5f8f194a575dcf70352bf9bd9490edeb232e45ca
Alex <azdc1986@gmail.com>
2024-03-08 11:56:19 +0100

    Config - Backend section

--- backend/src/main/java/com/az/base/BaseZApplication.java
+++ backend/src/main/java/com/az/base/BaseZApplication.java
@@ -1,12 +1,17 @@
 package com.az.base;
 
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableAdminServer
 @SpringBootApplication
 public class BaseZApplication {
-//    private static final Logger logger = LoggerFactory.getLogger(BaseZApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(BaseZApplication.class);
     public static void main(String[] args) {
+        logger.info("[AZ] - Starting ...");
         SpringApplication.run(BaseZApplication.class, args);
     }
 }
--- backend/src/test/java/com/az/base/BaseZApplicationTests.java
+++ backend/src/test/java/com/az/base/BaseZApplicationTests.java
@@ -1,4 +1,4 @@
-package com.az.main;
+package com.az.base;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
